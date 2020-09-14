068d5ae03daea6e72cde5428f4069d222a3e30eb
christhewon <chrislee752@yahoo.com>
2020-09-14 13:25:50 -0700

    finished the fretboard outline

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ button.addEventListener("click", hello)
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#000000";
-ctx.fillRect(0, 0, 1000, 10);
+ctx.fillRect(0,0,2000,5);
+ctx.fillRect(0,100,2000,5);
+
+ctx.fillRect(0,0,5,100);
+ctx.fillRect(100,0,5,100);
+ctx.fillRect(200,0,5,100);
+ctx.fillRect(300,0,5,100);
+ctx.fillRect(400,0,5,100);
+ctx.fillRect(500,0,5,100);
+ctx.fillRect(600,0,5,100);
+ctx.fillRect(700,0,5,100);
+ctx.fillRect(800,0,5,100);
+ctx.fillRect(900,0,5,100);
+ctx.fillRect(1000,0,5,100);
+ctx.fillRect(1100,0,5,100);
+ctx.fillRect(1200,0,5,100);
+ctx.fillRect(1300,0,5,100);
+ctx.fillRect(1400,0,5,100);
+ctx.fillRect(1500,0,5,100);
+ctx.fillRect(1600,0,5,100);
+ctx.fillRect(1700,0,5,100);
+ctx.fillRect(1800,0,5,100);
+ctx.fillRect(1900,0,5,100);
 
 
 function hello() {
