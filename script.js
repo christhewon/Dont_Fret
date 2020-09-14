3aa94ddf3a2c5d3dd72a18bcc66e959c6a88d3bf
christhewon <chrislee752@yahoo.com>
2020-09-14 13:17:26 -0700

    Added a bar on the fretboard

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 button = document.getElementById("click")
 button.addEventListener("click", hello)
 
+
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#000000";
+ctx.fillRect(0, 0, 1000, 10);
+
+
 function hello() {
     alert("Hello World")
 }
