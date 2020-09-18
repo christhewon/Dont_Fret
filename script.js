7e3f7c0165840f07da099e1659460c5bd21f3483
christhewon <chrislee752@yahoo.com>
2020-09-17 19:14:28 -0700

    Used for loop to draw fretboard

--- script.js
+++ script.js
@@ -2,25 +2,12 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "black";
 //forloop this please
+let loop;
+let increment = 0;
+for (loop = 0; loop < 18; loop++) {
+    ctx.fillRect(increment, 0, 5, 100);
+    increment += 100
+}
 ctx.fillRect(0, 0, 2000, 5);
 ctx.fillRect(0, 100, 2000, 5);
-ctx.fillRect(0, 0, 5, 100);
-ctx.fillRect(100, 0, 5, 100);
-ctx.fillRect(200, 0, 5, 100);
-ctx.fillRect(300, 0, 5, 100);
-ctx.fillRect(400, 0, 5, 100);
-ctx.fillRect(500, 0, 5, 100);
-ctx.fillRect(600, 0, 5, 100);
-ctx.fillRect(700, 0, 5, 100);
-ctx.fillRect(800, 0, 5, 100);
-ctx.fillRect(900, 0, 5, 100);
-ctx.fillRect(1000, 0, 5, 100);
-ctx.fillRect(1100, 0, 5, 100);
-ctx.fillRect(1200, 0, 5, 100);
-ctx.fillRect(1300, 0, 5, 100);
-ctx.fillRect(1400, 0, 5, 100);
-ctx.fillRect(1500, 0, 5, 100);
-ctx.fillRect(1600, 0, 5, 100);
-ctx.fillRect(1700, 0, 5, 100);
-ctx.fillRect(1800, 0, 5, 100);
-ctx.fillRect(1900, 0, 5, 100);
+
